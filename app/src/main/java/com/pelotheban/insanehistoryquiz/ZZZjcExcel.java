20f8cbc06346825c5365071db4c3f3ed8f402f97
SamuelPuchala <sam@satov.ca>
2020-02-11 17:16:14 -0500

    recycler view and excel work, scroll behaviour added to manual input

--- app/src/main/java/com/pelotheban/insanehistoryquiz/ZZZjcExcel.java
+++ app/src/main/java/com/pelotheban/insanehistoryquiz/ZZZjcExcel.java
@@ -1,4 +1,115 @@
 package com.pelotheban.insanehistoryquiz;
 
 public class ZZZjcExcel {
+
+    private String aaaqno;
+    private String bbbcategory;
+    private String cccquestion;
+    private String dddcorrectansw;
+    private String eeewrongans1;
+    private String fffwrongans2;
+    private String gggwrongans3;
+    private String hhhwrongans4;
+    private String iiiexpanded;
+
+
+
+
+
+    public ZZZjcExcel(String aaaqno, String bbbcategory, String cccquestion, String dddcorrectansw, String eeewrongans1,
+                      String fffwrongans2, String gggwrongans3, String hhhwrongans4, String iiiexpanded) {
+
+        this.aaaqno = aaaqno;
+        this.bbbcategory = bbbcategory;
+        this.cccquestion = cccquestion;
+        this.dddcorrectansw = dddcorrectansw;
+        this.eeewrongans1 = eeewrongans1;
+        this.fffwrongans2 = fffwrongans2;
+        this.gggwrongans3 = gggwrongans3;
+        this.hhhwrongans4 = hhhwrongans4;
+        this.iiiexpanded = iiiexpanded;
+
+    }
+
+    public String getAaaqno() {
+        return aaaqno;
+    }
+
+    public void setAaaqno(String aaaqno) {
+        this.aaaqno = aaaqno;
+    }
+
+    public String getBbbcategory() {
+        return bbbcategory;
+    }
+
+    public void setBbbcategory(String bbbcategory) {
+        this.bbbcategory = bbbcategory;
+    }
+
+    public String getCccquestion() {
+        return cccquestion;    }
+
+    public void setCccquestion(String cccquestion) {
+        this.cccquestion = cccquestion;
+
+    }
+
+    public String getDddcorrectansw() {
+        return dddcorrectansw;    }
+
+    public void setDddcorrectansw(String dddcorrectansw) {
+        this.dddcorrectansw = dddcorrectansw;
+
+    }
+    public String getEeewrongans1() {
+        return eeewrongans1;    }
+
+    public void setEeewrongans1(String eeewrongans1) {
+        this.eeewrongans1 = eeewrongans1;
+
+    }
+
+    public String getFffwrongans2() {
+
+        return fffwrongans2;
+    }
+
+    public void setFffwrongans2 (String fffwrongans2) {
+        this.fffwrongans2 = fffwrongans2;
+    }
+
+    public String getGggwrongans3() {
+        return gggwrongans3;    }
+
+    public void setGggwrongans3(String gggwrongans3) {
+        this.gggwrongans3 = gggwrongans3;
+
+    }
+
+    public String getHhhwrongans4() {
+        return hhhwrongans4;    }
+
+    public void setHhhwrongans4(String hhhwrongans4) {
+        this.hhhwrongans4 = hhhwrongans4;
+
+    }
+
+    public String getIiiexpanded() {
+        return iiiexpanded;    }
+
+    public void setIiiexpanded(String iiiexpanded) {
+        this.iiiexpanded = iiiexpanded;
+
+    }
+
+
+
+    public ZZZjcExcel(){
+
+    }
+
+
+
+
 }
